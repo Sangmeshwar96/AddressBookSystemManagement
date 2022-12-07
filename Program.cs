6e9d3bf6944f7890fa820f625ff1b6e543bba729
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-07 10:46:59 +0530

    Creating Contacts In Address Book System

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿Console.WriteLine("----Welcome to AddressBook System Program----");
-Console.ReadLine();
+﻿using AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----Welcome to AddressBook System Program----");
+            Console.WriteLine("Enter the details to create a contact");
+            Address obj = new Address();
+            obj.create();
+            obj.display();
+            //Console.ReadLine();
+        }
+    }
+}
