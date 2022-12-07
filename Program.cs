d957fa98a5a65b12eba39ce826fbeafd72ab3615
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-07 11:30:42 +0530

    Editing Contacts From Address Book System

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿Console.WriteLine("----Welcome to AddressBook System Program----");
-Console.ReadLine();
+﻿using AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----Welcome to AddressBook System Program----");
+            //Console.WriteLine("Enter the details to create a contact");
+            Console.WriteLine("Press 1 to add contact Press 2 to edit contact");
+            Address obj = new Address();
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Enter the number of contacts needed to add");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("Enter the details to create a contact");
+                        obj.create();
+                    }
+                    Console.WriteLine("These are the contacts stored");
+                    obj.display();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the first name of the contact you want to edit the details of");
+                    string Firstname = Console.ReadLine();
+                    Console.WriteLine("Enter the last name of the contact you want to edit the details of");
+                    string l_name = Console.ReadLine();
+                    obj.edit(Firstname, l_name);
+                    obj.display();
+                    break;
+            }
+            //Console.ReadLine();
+        }
+    }
+}
